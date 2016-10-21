2e2e67baad2d92c3689e38809519639da8aa6cfc
Nikola <nikola.golux@gmail.com>
2016-10-21 22:16:07 +0200

    assets precompiled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+burgers = Burger.create([
+	{ime: 'Mala pljeskavica', cena: '140'},
+	{ime: 'Srednja pljeskavica', cena: '160'}, 
+	{ime: 'Velika pljeskavica', cena: '190'},
+	{ime: 'Gurmanska pljeskavica', cena: '250'},
+	{ime: 'Punjena pljeskavica', cena: '250'},
+	{ime: 'Belo meso', cena: '230'},
+	{ime: 'Batak', cena: '220'},
+	{ime: 'Bum 2', cena: '600'},
+	{ime: 'Bum 4', cena: '1000'}	
+])
+
+pancakes = Pancake.create([
+	{ime: 'Eurocream palacinka', cena: '140'},
+	{ime: 'Slana palacinka', cena: '160'}, 
+	{ime: 'Radovan Karadzic palacinka', cena: '230'}
+])
+	
+
